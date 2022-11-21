4409c13592a5afc1273b2a64a185574ed8922cb2
AleksVecher1 <aliaksandr.vechar@inputhealth.com>
2022-11-21 13:26:02 +0300

    web project

---
+++ src/views/View.ts
@@ -0,0 +1,70 @@
+import { Model } from '../models/Model'
+import { HasId } from '../models/Model'
+
+
+export abstract class View<T extends Model<K>, K extends HasId> { // temporal solution for generic K, import the interface HasId and extends
+  abstract template(): string
+
+  regions: { [kiy: string]: Element} = {}
+
+  constructor(public parent: Element, public model: T){
+    this.bindModel()
+  }
+
+  regionsMap(): {[key: string] : string}{
+    return {}
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {}
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render()
+    })
+
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+  }
+
+  mapRegions(fragment: DocumentFragment):void {
+    const regionsMap = this.regionsMap()
+    for (let key in regionsMap) {
+      const selector = regionsMap[key]
+      const element = fragment.querySelector(selector)
+      if ( element ) {
+        this.regions[key] = element
+      }
+    }
+
+  }
+
+  onRender(): void {}
+
+  render(): void {
+    this.parent.innerHTML = ''
+
+    const templateElement = document.createElement('template')
+    templateElement.innerHTML = this.template()
+
+    this.bindEvents(templateElement.content)
+    this.mapRegions(templateElement.content)
+
+    this.onRender()
+
+    this.parent.append(templateElement.content)
+  }
+
+
+}
